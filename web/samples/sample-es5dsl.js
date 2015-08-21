3decf0cfd158ad744460dcdbee7b1c9b1394ee70
Valentyn Shybanov <olostan@gmail.com>
2015-08-21 09:43:25 +0300

    Some new changes

---
+++ web/samples/sample-es5dsl.js
@@ -0,0 +1,12 @@
+var AngularUApp = ng.
+    Component({ selector: 'angular-u-app' }).
+    View({ templateUrl: 'app.html' }).
+    Class({
+        constructor: function () {
+            this.name = 'World';
+        },
+        greet: function () {
+            alert('Hello ' + this.name);
+        }
+    });
+ng.bootstrap(AngularUApp);
